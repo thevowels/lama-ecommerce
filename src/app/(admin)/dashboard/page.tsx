1ca5ff015913dc193756a361ec327848bcca4819
Aung Phyo Htet <aungphyohtet1@gmail.com>
2026-03-12 06:02:28 +0700

    preparing for revenue chart.

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,7 +1,12 @@
-export default function Dashboard(){
-	return (
-		<div>
-			Dashboard Page
-		</div>
-	)
+export default function Dashboard() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-1"></div>
+      <div className="bg-primary-foreground p-4 rounded-lg"></div>
+      <div className="bg-primary-foreground p-4 rounded-lg"></div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-1"></div>
+      <div className="bg-primary-foreground p-4 rounded-lg"></div>
+      <div className="bg-primary-foreground p-4 rounded-lg"></div>
+    </div>
+  );
 }
