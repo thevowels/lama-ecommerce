00f50473866513190c2e9d223c9d5e861e84705c
Aung Phyo Htet <aungphyohtet1@gmail.com>
2026-03-12 10:54:03 +0700

    feat: integrating clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,13 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "./providers/theme-provider";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +37,24 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ClerkProvider>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+
           <TooltipProvider>{children}</TooltipProvider>
-          <ToastContainer position="bottom-right" />
+        </ClerkProvider>
+        <ToastContainer position="bottom-right" />
       </body>
     </html>
   );
